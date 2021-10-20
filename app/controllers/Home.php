70caa82b235f8f17ddaef3498e60bc5330978a67
regaasadarma <regaasadarmq@gmail.com>
2021-10-20 13:53:14 +0800

    MVC #4 Controller

---
+++ app/controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+
+class Home
+{
+    public function index()
+    {
+        echo "Home/index";
+    }
+
+    public function coba()
+    {
+        echo "home/coba";
+    }
+}
---
+++ app/controllers/User.php
@@ -0,0 +1,13 @@
+<?php
+
+class User
+{
+    public function index()
+    {
+        echo "User/index";
+    }
+    public function profile($nama = "Linux", $pekerjaan = "Devs")
+    {
+        echo "Salam kenal saya $nama, saya seorang $pekerjaan";
+    }
+}
