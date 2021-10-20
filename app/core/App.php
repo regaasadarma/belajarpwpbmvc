a2d475807fdfe7cb42f605d4cef2e509b5bac0c4
regaasadarma <regaasadarmq@gmail.com>
2021-10-20 13:56:01 +0800

    MVC #4 Routing

--- app/core/App.php
+++ app/core/App.php
@@ -2,20 +2,17 @@
 
 class App
 {
-
   protected $controller = 'Home';
   protected $method = 'index';
   protected $params = [];
-
-
 
   public function __construct()
   {
     $url = $this->parseURL();
 
     if ($url == NULL) {
-      $url =[$this->controller];
-      }
+      $url = [$this->controller];
+    }
 
     if (file_exists('../app/controllers/' . $url[0] . '.php')) {
       $this->controller = $url[0];
@@ -35,16 +32,15 @@ class App
     if (!empty($url)) {
       $this->params = array_values($url);
     }
-
     call_user_func_array([$this->controller, $this->method], $this->params);
   }
 
   public function parseURL()
   {
     if (isset($_GET['url'])) {
-      $url = rtrim($_GET['url'], ' / ');
+      $url = rtrim($_GET['url'], '/');
       $url = filter_var($url, FILTER_SANITIZE_URL);
-      $url = explode(' / ', $url);
+      $url = explode('/', $url);
       return $url;
     }
   }
