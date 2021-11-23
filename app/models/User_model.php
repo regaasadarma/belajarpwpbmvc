d47e6b8b87bd9d2657ef1e89edb643e1323c07ef
regaasadarma <regaasadarmq@gmail.com>
2021-11-23 22:44:19 +0800

    MVC #7 Model

---
+++ app/models/User_model.php
@@ -0,0 +1,8 @@
+<?php
+ class User_model {
+     private $nama = "Rega Asa Darma";
+     public function getUser() {
+         return $this->nama;
+     }
+ }
+?>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -25,7 +25,13 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container"><a class="navbar-brand" href="<?= BASE_URL; ?>">Sinau MVC</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav"><a class="nav-item nav-link active" href="<?= BASE_URL; ?>">Home <span class="sr-only">(current)</span></a><a class="nav-link" href="<?=BASE_URL?>/user/profile">Profile</a><a class="nav-item nav-link" href="<?= BASE_URL; ?>/user">User</a></div>
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="<?= BASE_URL; ?>">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="<?= BASE_URL ?>/blog">Blog</a>
+            <a class="nav-link" href="<?= BASE_URL ?>/user/profile">Profile</a>
+
+            <a class="nav-item nav-link" href="<?= BASE_URL; ?>/user">User</a>
+          </div>
         </div>
       </div>
     </nav>
